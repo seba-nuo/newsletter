cc8342f2e3e0412248c29a26e303b8c28690dd71
AntonioAlva1310 <smatechbeat@gmail.com>
2020-08-09 17:31:51 -0600

    Fixing Subscriptions Serializer

--- newsletters/urls.py
+++
@@ -1,2 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -6,7 +6,7 @@ from subscriptions.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ['id', 'user', 'subscription', 'created_at']
+        fields = ['id', 'user', 'newsletter', 'created']
 
 
 
