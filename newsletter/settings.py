b055364ddc7d5090e706c390da495e9571e4af2b
AntonioAlva1310 <smatechbeat@gmail.com>
2020-08-15 21:47:57 -0600

    Adding suscription email notification

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -148,3 +148,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pruebaspruebas2020aca@gmail.com'
+EMAIL_HOST_PASSWORD = 'Pruebas2020'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
-
+from django.core.mail import EmailMessage
 from subscriptions.models import Subscription
 from subscriptions.serializers import SubscriptionSerializer
 from rest_framework import viewsets
-
+from auths.models import User
+from django.core.mail import send_mail
 # Create your views here.
+
+def send_email():
+    email = EmailMessage(
+        'Title',
+        'Hola',
+        'pruebaspruebas2020aca@gmail.com',
+        ['smartechbeat@gmail.com'],
+    )
+    email.send()
 
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
 
+
+    def create(self, request, *args, **kwargs):
+        response = super(SubscriptionViewSet, self).create(request, *args, **kwargs)
+        send_email()  # sending mail
+        return response
