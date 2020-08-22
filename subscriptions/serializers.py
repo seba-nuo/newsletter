d61cd33a1bb41eecba6256dd6d6d0d6b1b81a890
AntonioAlva1310 <smatechbeat@gmail.com>
2020-08-21 21:01:35 -0600

    finding target before creating

--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from subscriptions.models import Subscription
+from newsletters.serializers import NewsletterSerializer
 
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    # newsletter = NewsletterSerializer(read_only=False)
+
+    class Meta:
+        model = Subscription
+        fields = ['id', 'user', 'newsletter', 'created']
+
+
+class SubscriptionDetailSerializer(serializers.ModelSerializer):
+    newsletter = NewsletterSerializer(read_only=False)
+
     class Meta:
         model = Subscription
         fields = ['id', 'user', 'newsletter', 'created']
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.core.mail import EmailMessage
 from subscriptions.models import Subscription
-from subscriptions.serializers import SubscriptionSerializer
+from subscriptions.serializers import SubscriptionSerializer, SubscriptionDetailSerializer
 from rest_framework import viewsets
 from auths.models import User
+from newsletters.models import Newsletter
 from django.core.mail import send_mail
 # Create your views here.
 
@@ -22,11 +23,30 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
     serializer_class = SubscriptionSerializer
 
 
+    # def get_queryset(self):
+        
+    #     subscriptions_filtered = Subject.objects.filter(**parameters)
+    #     return subjects_filtered
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return SubscriptionDetailSerializer
+        return SubscriptionSerializer
+
     def create(self, request, *args, **kwargs):
+        print(request.data)
         print(request.data['user'])
+        newsletter = Newsletter.objects.filter(id=request.data['newsletter'])
+        target_found = newsletter.values()
+        my_target = target_found[0]['target']
+        print(target_found)
+        print(my_target)
+        print(newsletter)
         user_id = request.data['user']
         user = User.objects.filter(id=user_id)
         print(user)
         response = super(SubscriptionViewSet, self).create(request, *args, **kwargs)
         send_email()  # sending mail
         return response
+#EN EL QUERYSET DE UNA FILTRAR SOLO LOS QUE LLEGUEN AL TARGET. 
+#TRAER EL SIZE DE LA LISTA. DE TODOS LOS VOTOS. FILTRADOS POR NEWSLETTER. 
